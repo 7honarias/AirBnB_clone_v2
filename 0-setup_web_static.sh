d36503d3e2619bcc17626db5156fd68357ccf8c6
7honarias <7honarias@gmail.com>
2021-04-15 21:07:30 +0000

    [ADD] - correction 3

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,5 +8,5 @@ sudo mkdir -p /data/web_static/shared
 echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sudo sed -i "42i\\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+sudo sed -i "42i\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 sudo service nginx start
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,33 +5,28 @@ function do_deploy:
 """
 
 from fabric.api import run, env, put
-from os.path import isdir
-env.hosts = ['35.196.67.203', '35.229.83.138']
+from os.path import exists
+env.hosts = ['ubuntu@35.196.67.203', 'ubuntu@35.229.83.138']
 
 
 def do_deploy(archive_path):
     """Returns False if the file at the
     path archive_path doesn’t exist
     """
-    if isdir(archive_path):
-        try:
-            file_name = archive_path.split("/")[-1]
-            with_out_ext = file_name.split('.')[0]
-            path = '/data/web_static/releases'
-            put(archive_path, "/tmp/")
-            run("mv {} ./tmp".format(archive_path))
-            run("mkdir -p {}/{}".format(path, with_out_ext))
-            run("tar -xzf /tmp/{} -C {}/{}"
-                .format(file_name, path, with_out_ext))
-            run("rm /tmp/{}".format(file_name))
-            run("mv {}/{}/web_static/* {}/{}"
-                .format(path, with_out_ext, path, with_out_ext))
-            run("rm -rf {}/{}/web_static")
-            run("rm -rf /data/web_static/current")
-            run("ln -s {}/{} /data/web_static/current"
-                .format(path, with_out_ext))
-        except:
-            return(False)
-    else:
-        return(False)
-    return(True)
+    if exists(archive_path) is False:
+        return False
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
